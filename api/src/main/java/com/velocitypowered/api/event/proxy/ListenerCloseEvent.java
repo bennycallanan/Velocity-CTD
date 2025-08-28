7d67fa1f013ee5cd5820fbe7977893dbb4d1c5d1
Brendan Callanan <brendan.business@outlook.com>
2025-08-28 20:43:54 +0100

    fix: 2424

--- api/src/main/java/com/velocitypowered/api/event/proxy/ListenerCloseEvent.java
+++ api/src/main/java/com/velocitypowered/api/event/proxy/ListenerCloseEvent.java
@@ -19,7 +19,7 @@ public final class ListenerCloseEvent {
   /**
    * The socket address the listener was bound to.
    */
-  private final InetSocketAddress address;
+  private final InetSocketAddress address; //afafaf
 
   /**
    * The type of listener being closed.
