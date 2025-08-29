b40f2841e814789cc23b62e5dfbc01fc6f5d0341
Brendan Callanan <brendan.business@outlook.com>
2025-08-29 11:12:53 +0100

    test #5

--- proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
+++ proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
@@ -179,7 +179,7 @@ public class VelocityServer implements ProxyServer, ForwardingAudience {
               .serializer().getAdapter(Component.class)
       )
       .registerTypeHierarchyAdapter(Favicon.class, FaviconSerializer.INSTANCE)
-      .create(); //dafaf
+      .create();
 
   /**
    * A {@link Gson} instance for serializing server ping responses for Minecraft versions
